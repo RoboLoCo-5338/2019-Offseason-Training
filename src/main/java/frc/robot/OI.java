b899cefcd9751a21de6070c12fd4497820eebc49
vjrocks5503 <emailvinay2@gmail.com>
2019-06-19 22:06:22 -0400

    Added OI

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,65 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * Add your docs here.
+ */
+public class OI {
+
+    // Create joysticks
+    private static final Joystick leftController = new Joystick(0);
+    private static final Joystick rightController = new Joystick(0);
+
+    // Create enum for different buttons
+    public enum Button {
+        LOL_1, LOL_2
+    }
+
+    public boolean get(final Button button) {
+        switch (button) {
+        case LOL_1:
+            return leftController.getRawButton(0);
+        case LOL_2:
+            return rightController.getRawButton(0);
+        default:
+            return false;
+        }
+    }
+
+    // Public method that returns the left joystick's deadzone-adjusted values
+    public double getLeftJoystick(final char input) {
+        switch (input) {
+        case 'X': // Gets deadzone corrected x-axis position
+            return (leftController.getRawAxis(0));
+        case 'Y': // Gets deadzone corrected y-axis position
+            return -(leftController.getRawAxis(1));
+        case 'Z': // Gets deadzone corrected z-axis (rotation) position
+            return 0.0;
+        default: // Returns 0.0 is argument is unknown
+            return 0.0;
+        }
+    }
+
+    // Public method that returns the right joystick's deadzone-adjusted values
+    public double getRightJoystick(final char input) {
+        switch (input) {
+        case 'X': // Gets deadzone corrected x-axis position
+            return (leftController.getRawAxis(4));
+        case 'Y': // Gets deadzone corrected y-axis position
+            return -(leftController.getRawAxis(5));
+        case 'Z': // Gets deadzone corrected z-axis (rotation) position
+            return 0.0;
+        default: // Returns 0.0 is argument is unknown
+            return 0.0;
+        }
+    }
+
+}
